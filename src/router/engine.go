77e1bf522570314b6e3f3f3a0cbee33297c2d24c
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-06-26 16:30:53 +0900

    wip

--- src/router/engine.go
+++ src/router/engine.go
@@ -5,15 +5,7 @@ import (
 	"github.com/evex-dev/hono.go/context"
 )
 
-type Params map[string]string
-
-type Context struct {
-	Write   http.ResponseWriter
-	Request *http.Request
-	Params  Params
-}
-
-type HandlerFunc func(*Context)
+type HandlerFunc func(*context.Context)
 
 type ContextMethod struct {
 	Method  string

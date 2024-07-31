8cce8002432837711e581f39932f9d7091f9a085
evorax <151723343+evorax@users.noreply.github.com>
2024-07-31 19:02:02 +0900

    update trie tree

--- src/server/trie.go
+++ src/server/trie.go
@@ -1,139 +1,247 @@
 package server
 
 import (
-	"fmt"
 	"regexp"
 	"strings"
 
 	"github.com/evex-dev/hono.go/src/context"
 )
 
-// structs
+type TrieFunc func(params map[string]string) (string, map[string]string)
+
+type Pattern struct {
+	Key     string
+	Name    string
+	Matcher *regexp.Regexp
+}
+
 type TrieNode struct {
-	children map[string]*TrieNode
-	isEnd    bool
-	paramKey string
-	regex    *regexp.Regexp
-	wildcard *TrieNode
+	Methods  map[string]TrieFunc
+	Children map[string]*TrieNode
+	Patterns []Pattern
+	Order    int
+	Name     string
+	Params   map[string]string
 }
 
-type Trie struct {
-	root     *TrieNode
-	patterns map[*TrieNode]string
+type ParamsValues struct {
+	Values []ParamsValue
 }
 
-// trie tree
-func NewTrie() *Trie {
-	return &Trie{
-		root: &TrieNode{
-			children: make(map[string]*TrieNode),
-		},
-		patterns: make(map[*TrieNode]string),
+type ParamsValue struct {
+	Path   string
+	Params map[string]string
+}
+
+type Value struct {
+	Handler TrieFunc
+	Params  map[string]string
+	Name    string
+	Score   int
+}
+
+func NewTrie() *TrieNode {
+	return &TrieNode{
+		Children: make(map[string]*TrieNode),
+		Methods:  make(map[string]TrieFunc),
+		Params:   make(map[string]string),
 	}
 }
 
-func (t *Trie) AddRoute(pattern string) {
-	parts := strings.Split(pattern, "/")[1:]
-	node := t.root
+func (t *TrieNode) getHandlerSets(nodeParams, params map[string]string) []Value {
+	var handlerSets []Value
 
-	for _, part := range parts {
-		if part == "*" {
-			if node.wildcard == nil {
-				node.wildcard = &TrieNode{children: make(map[string]*TrieNode)}
-			}
-			node = node.wildcard
-		} else if len(part) > 0 && part[0] == ':' {
-			paramKey := part[1:]
-			if openBracketIndex := strings.Index(paramKey, "{"); openBracketIndex != -1 {
-				regexPattern := paramKey[openBracketIndex+1 : len(paramKey)-1]
-				paramKey = paramKey[:openBracketIndex]
-				node = t.AddChild(node, paramKey, regexPattern)
-			} else {
-				node = t.AddChild(node, paramKey, "")
-			}
-		} else {
-			if _, ok := node.children[part]; !ok {
-				node.children[part] = &TrieNode{children: make(map[string]*TrieNode)}
-			}
-			node = node.children[part]
+	for _, handler := range t.Methods {
+		handlerSet := Value{
+			Handler: handler,
+			Params:  make(map[string]string),
+			Name:    t.Name,
+			Score:   t.Order,
 		}
-	}
 
-	node.isEnd = true
-	t.patterns[node] = pattern
-}
-
-func (t *Trie) AddChild(node *TrieNode, paramKey string, regexPattern string) *TrieNode {
-	for _, child := range node.children {
-		if child.paramKey == paramKey {
-			return child
+		for key, value := range nodeParams {
+			handlerSet.Params[key] = value
 		}
+		for key, value := range params {
+			handlerSet.Params[key] = value
+		}
+
+		handlerSets = append(handlerSets, handlerSet)
 	}
 
-	newNode := &TrieNode{
-		children: make(map[string]*TrieNode),
-		paramKey: paramKey,
-	}
-
-	if regexPattern != "" {
-		newNode.regex = regexp.MustCompile(regexPattern)
-	}
-
-	node.children[paramKey] = newNode
-	return newNode
+	return handlerSets
 }
 
-func (t *Trie) GetRoute(path string) (string, *context.Params) {
-	parts := strings.Split(path, "/")[1:]
-	node := t.root
+// AddRoute => Insert
+func (t *TrieNode) Insert(path string) {
+	t.Name = path
+	parts := splitRoutingPath(path)
+	curNode := t
 
-	params := make(context.Params)
-	var matchedPattern string
+	for i, p := range parts {
+		if p == "*" {
+			if _, exists := curNode.Children["*"]; !exists {
+				curNode.Children["*"] = NewTrie()
+			}
+			curNode = curNode.Children["*"]
+			if i == len(parts)-1 {
+				continue
+			}
+			continue
+		}
 
-	for _, part := range parts {
-		if _, ok := node.children[part]; ok {
-			node = node.children[part]
-		} else if node.wildcard != nil {
-			node = node.wildcard
-		} else {
-			matched := false
-			for _, child := range node.children {
-				if child.paramKey != "" {
-					if child.regex != nil && child.regex.MatchString(part) {
-						params[child.paramKey] = part
-						node = child
-						matched = true
-						break
-					} else if child.regex == nil {
-						params[child.paramKey] = part
-						node = child
-						matched = true
-						break
+		if child, exists := curNode.Children[p]; exists {
+			curNode = child
+			continue
+		}
+
+		child := NewTrie()
+		if pattern := getPattern(p); pattern != nil {
+			curNode.Patterns = append(curNode.Patterns, *pattern)
+		}
+		curNode.Children[p] = child
+		curNode = child
+	}
+
+	curNode.Methods["params"] = func(params map[string]string) (string, map[string]string) {
+		return path, params
+	}
+}
+
+func (t *TrieNode) Search(path string) []Value {
+	var sets []Value
+	t.Params = make(map[string]string)
+
+	curNodes := []*TrieNode{t}
+	parts := splitPath(path)
+
+	for i, part := range parts {
+		isLast := i == len(parts)-1
+		tempNodes := []*TrieNode{}
+
+		for _, node := range curNodes {
+			if nextNode, exists := node.Children[part]; exists {
+				nextNode.Params = node.Params
+
+				if isLast {
+					sets = append(sets, nextNode.getHandlerSets(nextNode.Params, map[string]string{})...)
+					if childNode, exists := nextNode.Children["*"]; exists {
+						sets = append(sets, childNode.getHandlerSets(nextNode.Params, map[string]string{})...)
+					}
+				} else {
+					tempNodes = append(tempNodes, nextNode)
+				}
+			}
+
+			for _, pattern := range node.Patterns {
+				if pattern.Matcher != nil {
+					if pattern.Matcher.MatchString(part) {
+						params := make(map[string]string)
+						params[pattern.Name] = part
+
+						if isLast {
+							sets = append(sets, node.getHandlerSets(params, node.Params)...)
+							if childNode, exists := node.Children[pattern.Key]; exists {
+								sets = append(sets, childNode.getHandlerSets(params, node.Params)...)
+							}
+						} else {
+							if childNode, exists := node.Children[pattern.Key]; exists {
+								childNode.Params = params
+							}
+						}
+					}
+				} else {
+					params := make(map[string]string)
+					params[pattern.Name] = part
+
+					if isLast {
+						sets = append(sets, node.getHandlerSets(params, node.Params)...)
+						if childNode, exists := node.Children[pattern.Key]; exists {
+							sets = append(sets, childNode.getHandlerSets(params, node.Params)...)
+						}
+					} else {
+						if childNode, exists := node.Children[pattern.Key]; exists {
+							childNode.Params = params
+						}
 					}
 				}
 			}
-			if !matched {
-				return "", nil
+
+			if wildcardNode, exists := node.Children["*"]; exists {
+				params := make(map[string]string)
+				if isLast {
+					sets = append(sets, wildcardNode.getHandlerSets(params, node.Params)...)
+				} else {
+					tempNodes = append(tempNodes, wildcardNode)
+				}
+			}
+		}
+
+		curNodes = tempNodes
+	}
+
+	return sortHandlerSets(sets)
+}
+
+func sortHandlerSets(sets []Value) []Value {
+	for i := 0; i < len(sets)-1; i++ {
+		for j := i + 1; j < len(sets); j++ {
+			if sets[i].Score > sets[j].Score {
+				sets[i], sets[j] = sets[j], sets[i]
 			}
 		}
 	}
+	return sets
+}
 
-	if node.isEnd {
-		matchedPattern = t.patterns[node]
-	} else if node.wildcard != nil {
-		matchedPattern = "/*"
+func splitPath(path string) []string {
+	return strings.Split(strings.Trim(path, "/"), "/")
+}
+
+func splitRoutingPath(path string) []string {
+	return splitPath(path)
+}
+
+func getPattern(part string) *Pattern {
+	if strings.HasPrefix(part, ":") {
+		paramName := strings.TrimPrefix(part, ":")
+		var matcher *regexp.Regexp
+		if strings.Contains(paramName, "{") && strings.Contains(paramName, "}") {
+			parts := strings.Split(paramName, "{")
+			regexPart := strings.TrimSuffix(parts[1], "}")
+			matcher = regexp.MustCompile(regexPart)
+			paramName = parts[0]
+		}
+		return &Pattern{
+			Key:     part,
+			Name:    paramName,
+			Matcher: matcher,
+		}
 	}
+	return nil
+}
 
-	return matchedPattern, &params
+// GetRoute => Find
+func (t *TrieNode) Find(path string) *ParamsValues {
+	sets := t.Search(path)
+	var values ParamsValues
+	for _, set := range sets {
+		path, pattern := set.Handler(set.Params)
+		values.Values = append(values.Values, ParamsValue{
+			Path:   path,
+			Params: pattern,
+		})
+	}
+	return &values
 }
 
 // Router
-// /* is not support, so coming soon by evorax
+// experimental support for /* by evorax
 func NewTrieRouter(routes *Routes) *Router {
 	trie := NewTrie()
 
 	for _, route := range routes.RouteList {
-		trie.AddRoute(route.Pattern)
+		trie.Insert(route.Pattern)
 	}
 
 	return &Router{
@@ -172,11 +280,18 @@ func MergeParams(params1, params2 *context.Params) *context.Params {
 	return params1
 }
 
-func (t *Trie) Compare(path, dst string) (*context.Params, bool) {
-	pattern, params := t.GetRoute(dst)
+func (t *TrieNode) Compare(path, dst string) (*context.Params, bool) {
+	values := t.Find(dst)
 
-	// debug
-	fmt.Println("[DEBUG]", pattern, *params, pattern == path, fmt.Sprintf("(%s == %s)", pattern, path))
+	params := make(context.Params)
 
-	return params, pattern == path
+	for _, value := range values.Values {
+		for key, value2 := range value.Params {
+			params[key] = value2
+		}
+
+		return &params, value.Path == path
+	}
+
+	return nil, false
 }

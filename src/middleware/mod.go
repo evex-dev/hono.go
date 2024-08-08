6872a5f5529870b677668356ea824d88a72936cc
skikozou <xxxskikozouxxx@gmail.com>
2024-08-09 08:16:52 +0900

    add middleware

---
+++ src/middleware/mod.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"mime"
+	"os"
+	"path/filepath"
+
+	"github.com/evex-dev/hono.go/src/context"
+	"github.com/evex-dev/hono.go/src/server"
+)
+
+func serveStatic(path string) server.HandlerFunc {
+	return func(ctx *context.Context) {
+		ctype := CheckType(path)
+		file, err := os.ReadFile(path)
+		if err != nil {
+			ctx.Status(404)
+			ctx.Text("404 not found")
+			ctx.End()
+			return
+		}
+		ctx.Status(200)
+		ctx.Res.Write(file)
+		ctx.AddHeader("Content-Type", ctype)
+		ctx.End()
+	}
+}
+
+func CheckType(path string) string {
+	ex := filepath.Ext(path)
+	return mime.TypeByExtension(ex)
+}
+
+func poweredBy() server.HandlerFunc {
+	return func(ctx *context.Context) {
+		ctx.AddHeader("X-Powered-By", "Hono.go")
+		ctx.Next()
+	}
+}
